207656711cdc3de50e336a24e6b5e747476bd9bc
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-05-10 14:48:33 -0400

    add to basic template

--- docfxmd.py
+++ docfxmd.py
@@ -19,6 +19,13 @@ def build_index(arr):
         count += 1
     return index
 
+def html_to_md(data):
+    result = ''
+    doc = etree.HTML(data)
+    for ele in doc[0]:
+        result += ele.text
+    return result
+
 def docfx_to_md(data):
     markdown = []
     items = data['items']
@@ -27,6 +34,7 @@ def docfx_to_md(data):
         'class',
         'constructor',
         'field',
+        'property',
         'method'
     ])
 
@@ -44,12 +52,19 @@ def docfx_to_md(data):
         if item_type == 'class':
             item_md.append('# Class %s' % name)
             item_md.append('')
+        elif item_type == 'constructor':
+            item_md.append('## Constructor')
+            item_md.append('')
+        elif item_type in ('field', 'property'):
+            item_md.append('### ' + item['id'])
+            item_md.append('')
+        elif item_type == 'method':
+            item_md.append('### ' + item['name'])
+            item_md.append('')
 
         summary = item.get('summary')
         if summary is not None:
-            doc = etree.HTML(summary)
-            for ele in doc[0]:
-                item_md.append(ele.text)
+            item_md.append(html_to_md(summary))
             item_md.append('')
 
         inheritance = item.get('inheritance')
@@ -66,35 +81,83 @@ def docfx_to_md(data):
                 item_md.append('- ' + member)
             item_md.append('')
 
-        namespace = item.get('namespace')
-        if namespace is not None:
-            item_md.append('Namespace: ' + namespace)
-            item_md.append('')
+        if item_type == 'class':
+            namespace = item.get('namespace')
+            if namespace is not None:
+                item_md.append('Namespace: ' + namespace)
+                item_md.append('')
 
-        assemblies = item.get('assemblies')
-        if assemblies is not None:
-            item_md.append('Assemblies:')
-            for asm in assemblies:
-                item_md.append('- ' + asm)
-            item_md.append('')
+            assemblies = item.get('assemblies')
+            if assemblies is not None:
+                item_md.append('Assemblies:')
+                for asm in assemblies:
+                    item_md.append('- ' + asm)
+                item_md.append('')
 
         syntax = item.get('syntax')
         if syntax is not None:
-            item_md.append('Syntax')
-            item_md.append('```')
+            if item_type == 'class':
+                item_md.append('Syntax')
+            else:
+                item_md.append('Declaration')
+
             if LANG == LANG_CS:
-                item_md.append(syntax['content'])
+                content_key = 'content'
+                lang = 'csharp'
             elif LANG == LANG_VB:
-                item_md.append(syntax['content.vb'])
+                content_key = 'content.vb'
+                lang = ''
+
+            item_md.append('```' + lang)
+            item_md.append(syntax[content_key])
             item_md.append('```')
+
+            parameters = syntax.get('parameters')
+            if parameters is not None and len(parameters) != 0:
+                has_description = any(map(lambda x: 'description' in x, parameters))
+                item_md.append('Parameters')
+                item_md.append('')
+                if has_description:
+                    item_md.append('| Type | Name | Description |')
+                    item_md.append('|---|---|---|')
+                    for param in parameters:
+                        item_md.append('| %s | %s | %s |' % (
+                            param['type'],
+                            param['id'],
+                            param['description'],
+                        ))
+                else:
+                    item_md.append('| Type | Name |')
+                    item_md.append('|---|---|')
+                    for param in parameters:
+                        item_md.append('| %s | %s |' % (param['type'], param['id']))
+
+            return_result = syntax.get('return')
+            if return_result is not None:
+                if item_type == 'property':
+                    item_md.append('Property Value')
+                else:
+                    item_md.append('Returns')
+                item_md.append('')
+
+                if 'description' in return_result:
+                    item_md.append('| Type | Description |')
+                    item_md.append('|---|---|')
+                    item_md.append('| %s | %s |' % (
+                        return_result['type'],
+                        html_to_md(return_result['description'])
+                    ))
+                else:
+                    item_md.append('| Type |')
+                    item_md.append('|---|')
+                    item_md.append('| %s |' % return_result['type'])
 
         remarks = item.get('remarks')
         if remarks is not None:
             doc = etree.HTML(remarks)
             item_md.append('Remarks')
             item_md.append('')
-            for ele in doc[0]:
-                item_md.append(ele.text)
+            item_md.append(html_to_md(remarks))
             item_md.append('')
 
         markdown.append(item_md)
@@ -102,7 +165,6 @@ def docfx_to_md(data):
     result = ''
     for item in markdown:
         result += '\n'.join(item) + '\n'
-    print(result)
 
     with open('test.md', 'w') as file:
         file.write(result)
