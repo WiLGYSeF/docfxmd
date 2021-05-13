a4b5f6ddced116adb24c48afac6a3431f1bb8257
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-05-13 12:36:33 -0400

    add newline_to_br() test

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -18,8 +18,16 @@ REPLACE_STRINGS = [
     },
 ]
 
+NEWLINE_TO_BR = {
+    'abc\n123': 'abc<br/>123'
+}
+
 
 class ConvertTest(unittest.TestCase):
     def test_replace_strings(self):
         for entry in REPLACE_STRINGS:
             self.assertEqual(convert.replace_strings(entry[STR], entry[REPL]), entry[VALUE])
+
+    def test_newline_to_br(self):
+        for key, val in NEWLINE_TO_BR.items():
+            self.assertEqual(convert.newline_to_br(key), val)
